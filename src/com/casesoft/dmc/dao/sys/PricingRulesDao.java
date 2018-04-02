d946bdd644d8415365b25d0b54ecaf87b0cd1fee
saber0123 <saber0123@mart>
2018-04-02 15:47:26 +0800

    Revert "代码同步"

---
+++ src/com/casesoft/dmc/dao/sys/PricingRulesDao.java
@@ -0,0 +1,10 @@
+package com.casesoft.dmc.dao.sys;
+
+import com.casesoft.dmc.core.dao.BaseHibernateDao;
+import com.casesoft.dmc.model.sys.PricingRules;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class PricingRulesDao extends BaseHibernateDao<PricingRules, String> {
+
+}
