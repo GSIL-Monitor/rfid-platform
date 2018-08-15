554e4556928ba7e78babf5a5576a56d839916a41
liutianci <1253947039@qq.com>
2018-08-15 12:43:05 +0800

    bug

--- src/com/casesoft/dmc/controller/sys/WarehouseController.java
+++ src/com/casesoft/dmc/controller/sys/WarehouseController.java
@@ -46,7 +46,10 @@ public class WarehouseController extends BaseController implements IBaseInfoCont
 	        page.setPageProperty();
 	        page = this.warehouseService.findPage(page,filters);
 	        for(Unit u:page.getRows()){
-	        	u.setUnitName(CacheManager.getUnitById(u.getOwnerId()).getName());
+				Unit unit = CacheManager.getUnitById(u.getOwnerId());
+				if (CommonUtil.isNotBlank(unit)){
+					u.setUnitName(CacheManager.getUnitById(u.getOwnerId()).getName());
+				}
 			}
 	        return page;
 	}
