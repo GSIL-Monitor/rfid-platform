630ad7f36d36b3062f30386fe5428192bea7dd13
anna <649314761@qq.com>
2018-08-28 11:27:43 +0800

    小程序销退单据管理，显示图片，增加ws调用方法

--- src/com/casesoft/dmc/controller/logistics/SaleOrderBillController.java
+++ src/com/casesoft/dmc/controller/logistics/SaleOrderBillController.java
@@ -4,6 +4,7 @@ package com.casesoft.dmc.controller.logistics;
 import com.alibaba.fastjson.JSON;
 import com.casesoft.dmc.cache.CacheManager;
 import com.casesoft.dmc.controller.pad.templatemsg.WechatTemplate;
+import com.casesoft.dmc.controller.product.StyleUtil;
 import com.casesoft.dmc.controller.task.TaskUtil;
 import com.casesoft.dmc.core.Constant;
 import com.casesoft.dmc.core.controller.BaseController;
@@ -79,7 +80,7 @@ public class SaleOrderBillController extends BaseController implements ILogistic
         return mv;
     }
 
-    @RequestMapping(value = "/page")
+    @RequestMapping(value = {"/page","/pageWS"})
     @ResponseBody
     public Page<SaleOrderBill> findPage(Page<SaleOrderBill> page, String userId) throws Exception {
         this.logAllRequestParams();
@@ -97,6 +98,66 @@ public class SaleOrderBillController extends BaseController implements ILogistic
         page = this.saleOrderBillService.findPage(page, filters);
         return page;
     }
+
+
+    /**
+     * add by Anna on 2018-08-28
+     * 小程序调用，获取销售单，带图片的信息
+     * @param pageSize
+     * @param pageNo
+     * @param userId
+     * @param sortOrder
+     * @return
+     */
+    @RequestMapping(value = "/findSaleOrderListWS")
+    @ResponseBody
+    public MessageBox findSaleOrderListWS(String pageSize, String pageNo, String userId, String sortOrder){
+        this.logAllRequestParams();
+        List<PropertyFilter> filters = PropertyFilter.buildFromHttpRequest(this.getRequest());
+        //权限设置，增加过滤条件，只显示当前ownerId下的销售单信息
+        User CurrentUser = CacheManager.getUserById(userId);
+        String ownerId = CurrentUser.getOwnerId();
+        //只要是管理员权限的账号就可以看所有单据
+//        String id = CurrentUser.getId();
+        if (!ownerId.equals("1")) {
+            PropertyFilter filter = new PropertyFilter("EQS_ownerId", ownerId);
+            filters.add(filter);
+        }
+        Page<SaleOrderBill> page = new Page<SaleOrderBill>();
+        page.setPageSize(Integer.parseInt(pageSize));
+        page.setPageNo(Integer.parseInt(pageNo));
+        page.setPage(Integer.parseInt(pageNo));
+        String sort = "billDate";
+        String order = "desc";
+        if (CommonUtil.isNotBlank(sortOrder)) {
+            String[] split = sortOrder.split("_");
+            sort = split[0];
+            order = split[1];
+        }
+        page.setSort(sort);
+        page.setOrder(order);
+        page.setPageProperty();
+        page = this.saleOrderBillService.findPage(page, filters);
+        String rootPath = this.getSession().getServletContext().getRealPath("/");
+        if (CommonUtil.isNotBlank(page.getRows())) {
+            for (SaleOrderBill saleOrderBill : page.getRows()) {
+
+                List<SaleOrderBillDtl> dtlList = this.saleOrderBillService.findBillDtlByBillNo(saleOrderBill.getBillNo());
+
+                for (SaleOrderBillDtl saleOrderBillDtl : dtlList) {
+                    String imgUrl = StyleUtil.returnImageUrl(saleOrderBillDtl.getStyleId(), rootPath);
+                    saleOrderBillDtl.setImgUrl(imgUrl);
+                    Style style = CacheManager.getStyleById(saleOrderBillDtl.getStyleId());
+                    if (CommonUtil.isNotBlank(style)) {
+                        saleOrderBillDtl.setStyleName(style.getStyleName());
+                    }
+                }
+                saleOrderBill.setDtlList(dtlList);
+            }
+        }
+        return new MessageBox(true, "success", page);
+    }
+
 
     @Override
     public Page<SaleOrderBill> findPage(Page<SaleOrderBill> page) throws Exception {
