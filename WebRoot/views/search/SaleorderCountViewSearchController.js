d9e347fcaa0cf730ed8732e42bf481e9de0e8fb1
liutianci <1253947039@qq.com>
2018-09-13 09:41:44 +0800

    销售报表筛选条件选不上

--- WebRoot/views/search/SaleorderCountViewSearchController.js
+++ WebRoot/views/search/SaleorderCountViewSearchController.js
@@ -2251,7 +2251,6 @@ function openSearchGuestDialog() {
     dialogOpenPage = "saleOrderReturn";
     prefixId="search";
     $("#modal_guest_search_table").modal('show').on('shown.bs.modal', function () {
-        prefixId="add";
         initGuestSelect_Grid();
     });
     $("#searchGuestDialog_buttonGroup").html("" +
